38051cf3c30061737deb46e1d64f2216af81eafd
joseGdaSilva <168657326+joseGdaSilva@users.noreply.github.com>
2024-05-25 14:32:30 -0300

    Versão final do protótipo
    Configurações finais do servidor
    Criação da pagina inicial
    Ajustes finais nos modelos e demais arquivos de configuração do Django

--- BrainSalesPlatform/Vendas/models.py
+++ BrainSalesPlatform/Vendas/models.py
@@ -6,7 +6,7 @@ class Cliente(models.Model):
     email = models.EmailField()
     telefone = models.CharField(max_length=20)
     def __str__(self):
-        return self.nome
+        return self.nome + f" (Id: {self.id})"
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
@@ -14,14 +14,14 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     quantidade_disponivel = models.IntegerField()
     def __str__(self):
-        return self.nome
+        return self.nome + f" (Id: {self.id})"
 
 class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     data_pedido = models.DateField()
     total_pedido = models.DecimalField(max_digits=10, decimal_places=2)
     def __str__(self):
-        return f"Pedido de {self.cliente.nome} em {self.data_pedido}"
+        return f"(Id: {self.id}) Pedido de {self.cliente.nome} {self.data_pedido.day:02d}/{self.data_pedido.month:02d}/{self.data_pedido.year} (R$ {self.total_pedido})"
 
 class ItemPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
@@ -30,7 +30,7 @@ class ItemPedido(models.Model):
     preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
     total_item = models.DecimalField(max_digits=10, decimal_places=2)
     def __str__(self):
-        return f"Item #{self.id} ({self.quantidade}x {self.produto.nome})"
+        return f"(Id: {self.id}) (Id pedido: {self.pedido.id}) - {self.quantidade}x {self.produto.nome}"
 
 class GestaoEstoque(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
@@ -38,4 +38,4 @@ class GestaoEstoque(models.Model):
     quantidade_saida = models.IntegerField()
     data_movimentacao = models.DateField()
     def __str__(self):
-        return f"Movimento de {self.produto.nome} em {self.data_movimentacao}"
+        return f"Movimento de {self.produto.nome} em {self.data_movimentacao.day:02d}/{self.data_movimentacao.month:02d}/{self.data_movimentacao.year}"
